a012fc405033563a08b3ba5dce4ca571badff1da
Shivam5022 <shivamverma5022@gmail.com>
2022-12-29 15:34:45 +0530

    updated small segtree implementation

--- algo/segtree_small.cpp
+++ algo/segtree_small.cpp
@@ -1,31 +1,31 @@
-// iterative segment tree with only point update
-struct SGT {
-  std::vector<int> seg; // O based
-  int n;
+struct SGT {   //segtree implementation by geothermal
+  std::vector<int> seg; 
+  int SZ;
   SGT() {};
-  void clear(){
-    n = 0;
-    seg.clear();
-  }
+  int combine (int a, int b) { return max(a, b); } // change to sum, max, min
   void build (vector<int> val) {
-    n = (int) val.size();
-    seg.resize(2 * n + 1);
-    for(int i = n; i < 2 * n; ++i) {
-      seg[i] = val[i - n];
+    SZ = (int) val.size();
+    seg.resize(2 * SZ + 1); // put identity here
+    for(int i = SZ; i < 2 * SZ; ++i) {
+      seg[i] = val[i - SZ];
     }
-    for(int i = n - 1; i > 0; --i) seg[i] = (seg[i << 1] + seg[(i << 1) | 1]);  //1
+    for(int i = SZ - 1; i > 0; --i) seg[i] = combine(seg[i << 1], seg[(i << 1) | 1]); 
   }
   void update (int p, int v) {
-    p += n; seg[p] += v;  // for ADDING to point, seg[p] += v & for SETTING, seg[p] = v.
-    for(int i = p; i > 1; i >>= 1) seg[i >> 1] = (seg[i] + seg[i ^ 1]);  //2
+    p += SZ; seg[p] = v;  // for ADDING to point, seg[p] += v & for SETTING, seg[p] = v.
+    for(int i = p; i > 1; i >>= 1) seg[i >> 1] = combine(seg[i], seg[i ^ 1]); 
+  }
+  int get (int idx) {
+    return seg[idx + SZ];
   }
   int query (int l, int r) {
     int ans = 0;  // set this to identity
-    l = max(0, l);
-    r = min(n - 1, r);
-    for(l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) ans = (ans + seg[l++]);  //3
-      if (r & 1) ans = (ans + seg[--r]);  //4
+    if (l > r) return ans;
+    l = max(0LL, l);
+    r = min(SZ - 1, r);
+    for(l += SZ, r += SZ + 1; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) ans = combine(ans, seg[l++]); 
+      if (r & 1) ans = combine(ans, seg[--r]);  
     }
     return ans;
   }
